3b69dc2105c25a1be73baf4fb1d061623a107aa7
wycheng <wychengpublic@163.com>
2017-12-03 22:05:52 +0800

    fix the mapper url addr in News.xml

--- src/com/irs_news/controller/NewsController.java
+++ src/com/irs_news/controller/NewsController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
